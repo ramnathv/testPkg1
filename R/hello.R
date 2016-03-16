cb4a2969ccd0d674fa86ec335d8fb25a6977516b
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-03-16 12:10:16 -0700

    update functions

--- R/hello.R
+++ R/hello.R
@@ -1,17 +1,7 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
-hello <- function(user) {
+#' Simple Hello World function
+#'
+#' @param user name of user to greet
+#' @export
+hello <- function(user = "World") {
   paste("Hello ", user, sep = ", ")
 }
