c21bb38d7fda576371182dbf4bfbe87a72f2468f
Chris Freeman <cfreeman.code@gmail.com>
2016-03-16 16:24:47 -0500

    Fixed docs

--- R/testing.R
+++ R/testing.R
@@ -1,5 +1,7 @@
-#' Testing
-#' @param base
+#' @title Square all the things
+#' @name Squared
+#' @description A function for squaring things
+#' @param base The base
 #' @export
 squared <- function(base) {
   return(base ** 2)
